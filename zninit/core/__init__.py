ad5c511e4169215507aa596b08fe621e206385d5
PythonFZ <fabian.zills@web.de>
2023-04-14 11:54:38 +0200

    allow args

--- zninit/core/__init__.py
+++ zninit/core/__init__.py
@@ -54,6 +54,40 @@ def get_init_type_error(
     )
 
 
+def _handle_args(args, kwargs, kwarg_names, cls_name):
+    """Add args to kwargs dict.
+
+    Parameters
+    ----------
+    args: list
+        list of positional arguments
+    kwargs: dict
+        dict of keyword arguments
+    kwarg_names: list
+        list of keyword names
+    cls_name: str
+        name of the class
+
+
+    Raises
+    ------
+    TypeError
+        if more positional arguments are given than keyword arguments
+        if a keyword argument is given multiple times
+    """
+    if len(args) > len(kwarg_names):
+        raise TypeError(
+            f"{cls_name}.__init__() takes {len(kwarg_names)} positional arguments"
+            f" but {len(args)} were given"
+        )
+
+    if args:
+        for idx, arg_value in enumerate(args):
+            if kwarg_names[idx] in kwargs:
+                raise TypeError(f"got multiple values for argument '{kwarg_names[idx]}'")
+            kwargs[kwarg_names[idx]] = arg_value
+
+
 def get_auto_init(
     kwargs_no_default: typing.List[str],
     kwargs_with_default: dict,
@@ -89,6 +123,10 @@ def get_auto_init(
         if args and not allow_args:
             raise get_args_type_error(args, cls_name, uses_auto_init)
         log.debug(f"The '__init__' uses auto_init: {uses_auto_init}")
+
+        kwarg_names = kwargs_no_default + list(kwargs_with_default)
+        _handle_args(args, kwargs, kwarg_names, cls_name)
+
         for kwarg_name in kwargs_no_default:
             try:  # pylint: disable=R8203
                 init_kwargs[kwarg_name] = kwargs.pop(kwarg_name)
